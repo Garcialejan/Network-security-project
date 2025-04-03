b71be640c56c852295e8ef395a91f70ac7c254e3
Alejandro_pc\Alejandro <garcialejan@gmail.com>
2025-04-03 20:37:24 +0200

    Solving problems for data ingestion

--- main.py
+++ main.py
@@ -9,10 +9,12 @@ import sys
 
 if __name__=='__main__':
     try:
-        trainingpipelineconfig=TrainingPipelineConfig()
-        dataingestionconfig=DataIngestionConfig(trainingpipelineconfig)
-        data_ingestion=DataIngestion(dataingestionconfig)
+        training_pipeline_config=TrainingPipelineConfig()
+        data_ingestion_config=DataIngestionConfig(training_pipeline_config)
+        data_ingestion=DataIngestion(data_ingestion_config)
         logger.info("Initiate the data ingestion")
+        data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+        print(data_ingestion_artifact)
         
     except Exception as e:
            raise NetworkSecurityException(e,sys)
