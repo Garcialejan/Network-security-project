0e3864bedf4034275870b53185fe3a50770e96ca
Alejandro_pc\Alejandro <garcialejan@gmail.com>
2025-10-12 13:58:32 +0200

    Model trainning pipeline finished

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -7,12 +7,19 @@ from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.utils.main_utils.utils import save_object, load_object, load_numpy_array_data
 from networksecurity.utils.ml_utils.model.estimator import NetworkModel
 from networksecurity.utils.ml_utils.metric.classification_metric import get_classification_score
-# from networksecurity.utils.ml_utils.model.evaluation import evaluate_models
+from networksecurity.utils.ml_utils.model.evaluation import evaluate_models
 
 import os
 import sys
 from pathlib import Path
-
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import (
+    AdaBoostClassifier,
+    GradientBoostingClassifier,
+    RandomForestClassifier
+)
+from xgboost import XGBClassifier 
 
 class ModelTrainer:
     def __init__(self,
@@ -25,11 +32,99 @@ class ModelTrainer:
         except Exception as e:
             raise NetworkSecurityException(e, sys)
         
-    def train_model(self, X_train, y_train):
-        pass
+    def train_model(self, X_train, y_train, X_test, y_test):
+        '''
+        Function to train the model with hyperparameter tunning
+        and create the model trainer artifact for the predictions.
+        '''
+        models = {
+                "Decision Tree": DecisionTreeClassifier(),
+                "Random Forest": RandomForestClassifier(verbose=0),
+                "Gradient Boosting": GradientBoostingClassifier(verbose=0),
+                "Logistic Regression": LogisticRegression(verbose=0),
+                "AdaBoost": AdaBoostClassifier(),
+                "XGBoostClassifier": XGBClassifier()
+            }
+        
+        params={
+            "Decision Tree": {
+                'criterion':['gini', 'entropy', 'log_loss'],
+                # 'splitter':['best','random'],
+                'max_features':['sqrt','log2'],
+            },
+            "Random Forest":{
+                # 'criterion':['gini', 'entropy', 'log_loss'],
+                
+                'max_features':['sqrt','log2',None],
+                'n_estimators': [8,16,32,128,256]
+            },
+            "Gradient Boosting":{
+                # 'loss':['log_loss', 'exponential'],
+                'learning_rate':[.1,.01,.05,.001],
+                'subsample':[0.6,0.7,0.75,0.85,0.9],
+                # 'criterion':['squared_error', 'friedman_mse'],
+                'n_estimators': [8,16,32,64,128,256],
+                'max_depth': [3, 6, 10]
+            },
+            "Logistic Regression":{},
+            "AdaBoost":{
+                'learning_rate':[.1,.01,.001],
+                'n_estimators': [8,16,32,64,128,256]
+            },
+            "XGBoostClassifier":{
+                'learning_rate':[0.1, 0.2, 0.3],
+                'max_depth':[3, 6, 10],
+                'lambda':[0, 1, 5, 10],
+                'alpha':[0, 1, 5, 10]
+            }
+            
+        }
+        
+        # We use GridSearch to find the best model with the best
+        # hyperparameters
+        model_report, best_params_per_model = evaluate_models(X_train=X_train,y_train=y_train,
+                                                              X_test=X_test,y_test=y_test,
+                                                              models=models,param=params)
+        
+        ## To get best model score from dict
+        best_model_score = max(sorted(model_report.values()))
+        ## To get best model name from dict
+        best_model_name = list(model_report.keys())[
+            list(model_report.values()).index(best_model_score)
+        ]
+        ## To get best model params from dict
+        best_params = best_params_per_model[best_model_name]
+        logger.info(f"Best model selected: {best_model_name}")
+        logger.info(f"Best hyperparameters: {best_params}")
+        logger.info(f"Best score: {best_model_score:.3f}")
+        
+        best_model = models[best_model_name]
+        y_train_pred=best_model.predict(X_train)
+        classification_train_metric=get_classification_score(y_true=y_train,y_pred=y_train_pred)
+        
+        y_test_pred=best_model.predict(X_test)
+        classification_test_metric=get_classification_score(y_true=y_test,y_pred=y_test_pred)
+        
+        preprocessor = load_object(file_path = self.data_transformation_artifact.transformed_object_file_path)
+        model_dir_path = os.path.dirname(self.model_trainer_config.trained_model_file_path)
+        os.makedirs(model_dir_path,exist_ok = True)
+
+        Network_Model = NetworkModel(preprocessor = preprocessor, model = best_model)
+        save_object(self.model_trainer_config.trained_model_file_path,obj = NetworkModel)
+        # model pusher
+        save_object("final_model/model.pkl",best_model)
+        
+        ## Model Trainer Artifact
+        model_trainer_artifact = ModelTrainerArtifact(
+            trained_model_file_path = self.model_trainer_config.trained_model_file_path,
+            train_metric_artifact = classification_train_metric,
+            test_metric_artifact = classification_test_metric
+            )
+        logger.info(f"Model trainer artifact: {model_trainer_artifact}")
+        return model_trainer_artifact
         
     def initiate_model_trainer(self) -> ModelTrainerArtifact:
-        try: 
+        try:
             train_file_path = self.data_transformation_artifact.transformed_train_file_path
             test_file_path = self.data_transformation_artifact.transformed_test_file_path
             
@@ -44,6 +139,8 @@ class ModelTrainer:
                 test_arr[:, -1]
             )
             
-            model = self.train_model(X_train, y_train)
+            model_trainer_artifact=self.train_model(X_train,y_train,X_test,y_test)
+            return model_trainer_artifact
+        
         except Exception as e:
             raise NetworkSecurityException(e, sys)
--- networksecurity/utils/ml_utils/model/evaluation.py
+++ networksecurity/utils/ml_utils/model/evaluation.py
@@ -1,5 +1,5 @@
 import sys
-from sklearn.metrics import r2_score
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import GridSearchCV
 
 from networksecurity.exception.exception import NetworkSecurityException
@@ -7,30 +7,28 @@ from networksecurity.exception.exception import NetworkSecurityException
 def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
+        best_params_per_model = {}
 
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
-            para=param[list(models.keys())[i]]
+        for model_name, model in models.items():
+            para = param.get(model_name, {})
 
-            gs = GridSearchCV(model,para,cv=3)
-            gs.fit(X_train,y_train)
-
-            model.set_params(**gs.best_params_)
-            model.fit(X_train,y_train)
-
-            #model.fit(X_train, y_train)  # Train model
-
-            y_train_pred = model.predict(X_train)
+            if para:
+                gs = GridSearchCV(model, para, cv=3)
+                gs.fit(X_train, y_train)
+                best_params = gs.best_params_
+                model.set_params(**best_params)
+                model.fit(X_train, y_train)
+            else:
+                model.fit(X_train, y_train)
+                best_params = {}
 
             y_test_pred = model.predict(X_test)
+            test_model_score = accuracy_score(y_test, y_test_pred)
 
-            train_model_score = r2_score(y_train, y_train_pred)
+            report[model_name] = test_model_score
+            best_params_per_model[model_name] = best_params
 
-            test_model_score = r2_score(y_test, y_test_pred)
-
-            report[list(models.keys())[i]] = test_model_score
-
-        return report
+        return report, best_params_per_model
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
