cef800fc57ac5a51120e59771c8f7f4ec6dfd960
Alejandro_pc\Alejandro <garcialejan@gmail.com>
2025-10-12 16:30:25 +0200

    Trac experiments with mlflow in local

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -12,6 +12,7 @@ from networksecurity.utils.ml_utils.model.evaluation import evaluate_models
 import os
 import sys
 from pathlib import Path
+from sklearn.base import BaseEstimator
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import (
@@ -19,7 +20,12 @@ from sklearn.ensemble import (
     GradientBoostingClassifier,
     RandomForestClassifier
 )
-from xgboost import XGBClassifier 
+from xgboost import XGBClassifier
+from xgboost import XGBModel
+
+import mlflow
+from mlflow.models import infer_signature
+from urllib.parse import urlparse
 
 class ModelTrainer:
     def __init__(self,
@@ -31,6 +37,61 @@ class ModelTrainer:
             
         except Exception as e:
             raise NetworkSecurityException(e, sys)
+        
+    def track_mlflow(self, best_model, classification_train_metric):
+        mlflow.set_tracking_uri("http://127.0.0.1:5000")
+        mlflow.set_experiment("Best model NetworkSecurity")
+        
+        with mlflow.start_run(run_name = "Best_model_params"):
+            # Set a tag that we can use to remind ourselves what this run was for
+            mlflow.set_tag("Training Info", "Best model from hyperparameter-tunning with Grid Search")
+                                  
+            mlflow.log_metrics({
+                "accuracy": classification_train_metric.accuracy_score,
+                "f1_score": classification_train_metric.f1_score,
+                "precision_score": classification_train_metric.precision_score,
+                "recall_score": classification_train_metric.recall_score,
+                })
+            # I want to know if the model is a sklearn model or a xgb model
+            model_name = best_model.__class__.__name__
+            registered_model_name = model_name
+            
+            mlflow.sklearn.log_model(best_model, "model")
+            tracking_url_type_store=urlparse(mlflow.get_tracking_uri()).scheme
+            # Model registry does not work with file store
+            if tracking_url_type_store != "file":
+                # Register the model
+                # There are other ways to use the Model Registry, which depends on the use case,
+                # please refer to the doc for more information:
+                # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+                if isinstance(best_model, XGBModel):
+                    # XGBoost model
+                    mlflow.xgboost.log_model(
+                        xgb_model=best_model,
+                        artifact_path="model",
+                        registered_model_name=registered_model_name
+                    )
+                elif isinstance(best_model, BaseEstimator):
+                    # Scikit-learn model (DecisionTree, RandomForest, etc.)
+                    mlflow.sklearn.log_model(
+                        sk_model=best_model,
+                        artifact_path="model",
+                        registered_model_name=registered_model_name
+                    )
+            else:
+                if isinstance(best_model, XGBModel):
+                    mlflow.xgboost.log_model(best_model, "model")
+                elif isinstance(best_model, BaseEstimator):
+                    mlflow.sklearn.log_model(best_model, "model")
+                    
+            # mlflow.log_param("best_n_estimators",best_params['n_estimators'])
+            # mlflow.log_param("best_max_depth", best_params['max_depth'])
+            # mlflow.log_param("best_min_samples_split", best_params['min_samples_split'])
+            # mlflow.log_param("best_min_samples_leaf", best_params['min_samples_leaf'])
+            # mlflow.log_metric("mse",mse)
+          
+            # signature=infer_signature(X_train, lr.predict(X_train))
+
         
     def train_model(self, X_train, y_train, X_test, y_test):
         '''
@@ -56,32 +117,31 @@ class ModelTrainer:
                 # 'criterion':['gini', 'entropy', 'log_loss'],
                 
                 'max_features':['sqrt','log2',None],
-                'n_estimators': [8,16,32,128,256]
+                'n_estimators': [32,128,256]
             },
             "Gradient Boosting":{
                 # 'loss':['log_loss', 'exponential'],
                 'learning_rate':[.1,.01,.05,.001],
                 'subsample':[0.6,0.7,0.75,0.85,0.9],
                 # 'criterion':['squared_error', 'friedman_mse'],
-                'n_estimators': [8,16,32,64,128,256],
+                'n_estimators': [32,64,128,256],
                 'max_depth': [3, 6, 10]
             },
             "Logistic Regression":{},
             "AdaBoost":{
                 'learning_rate':[.1,.01,.001],
-                'n_estimators': [8,16,32,64,128,256]
+                'n_estimators': [32,64,128,256]
             },
             "XGBoostClassifier":{
-                'learning_rate':[0.1, 0.2, 0.3],
+                'learning_rate':[0.01, 0.05, 0.1, 0.2, 0.3],
                 'max_depth':[3, 6, 10],
-                'lambda':[0, 1, 5, 10],
-                'alpha':[0, 1, 5, 10]
+                'lambda':[0, 0.1, 1, 3],
+                'alpha':[0, 0.1, 1, 3]
             }
             
         }
         
-        # We use GridSearch to find the best model with the best
-        # hyperparameters
+        # We use GridSearch to find the best model with the best hyperparameters
         model_report, best_params_per_model = evaluate_models(X_train=X_train,y_train=y_train,
                                                               X_test=X_test,y_test=y_test,
                                                               models=models,param=params)
@@ -101,9 +161,13 @@ class ModelTrainer:
         best_model = models[best_model_name]
         y_train_pred=best_model.predict(X_train)
         classification_train_metric=get_classification_score(y_true=y_train,y_pred=y_train_pred)
+        #* Track the trainning with MLFlow
+        self.track_mlflow(best_model, classification_train_metric)
         
         y_test_pred=best_model.predict(X_test)
         classification_test_metric=get_classification_score(y_true=y_test,y_pred=y_test_pred)
+        #* Track the test with MLFlow
+        self.track_mlflow(best_model, classification_test_metric)
         
         preprocessor = load_object(file_path = self.data_transformation_artifact.transformed_object_file_path)
         model_dir_path = os.path.dirname(self.model_trainer_config.trained_model_file_path)
